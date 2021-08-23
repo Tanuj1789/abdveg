b8c3812b5ac6890625fb53b1011d3f041d2bb832
Tanuj1789 <58630657+Tanuj1789@users.noreply.github.com>
2021-08-23 14:09:30 +0530

    removing mime error

--- abdveggies/settings.py
+++ abdveggies/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z=un5&ugs&l(oa$hmo$=detp@aauypkzl95da@1fj$9(nl0327'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
